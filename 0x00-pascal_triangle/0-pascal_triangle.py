821b9f0a9a56456f0acf247f4aa16b5125cd2cc4
Rational-Being <overcomer021@gmail.com>
2024-04-25 07:42:13 +0100

    pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,11 +3,12 @@
 python script that prints the pascal's trinagle
 """
 
+
 def pascal_triangle(n):
     """
     ccreates a pascal triangle
     """
-    
+
     if n <= 0:
         return []
     elif n == 1:
@@ -16,9 +17,8 @@ def pascal_triangle(n):
         result = [[1], [1, 1]]
         for i in range(2, n):
             row = [1]
-            for j in range(len(result[i-1]) - 1):
-                row.append(result[i-1][j] + result[i-1][j+1])
+            for j in range(len(result[i - 1]) - 1):
+                row.append(result[i - 1][j] + result[i - 1][j + 1])
             row.append(1)
             result.append(row)
-
         return result
