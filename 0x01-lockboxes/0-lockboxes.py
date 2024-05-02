4c20a2df53836a23a63955f93506a6346c56aa72
Rational-Being <overcomer021@gmail.com>
2024-05-02 17:27:27 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+'''
+A python method that determines if all boxes can be opened
+'''
+
+def canUnlockAll(boxes):
+    '''
+    Determines if all boxes can be unlocked by using the keys in the first box
+    '''
+    seen = set()
+    unseen = [0]
+
+    while unseen:
+        present =  unseen.pop()
+
+        if present in seen:
+            continue
+
+        seen.add(present)
+
+        for key in boxes[present]:
+            if key in seen:
+                continue
+            unseen.append(key)
+    
+    return len(seen) == len(boxes)
