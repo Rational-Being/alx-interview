e9f9b49734f3ddde0ac27eb0776aff6177d2951a
Rational-Being <overcomer021@gmail.com>
2024-05-09 09:58:59 +0100

    new method

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,13 +11,15 @@ def minOperations(n):
     if n < 0:
         return 0
 
-    else:
-        ops = 0
-        while n > 0:
-            if n % 2 == 0:
-                n = n // 2
-            else:
-                n = n - 1
-                ops += 1
-        return ops
+    ops_2 = 2
+    ops_1 = 1
+    ops_d = ops_1 / 2
+    ops = [0] * (n + 1)
 
+    for i in range(ops_2, n + 1):
+        ops[i] = i
+        for _ in range(ops_1, int(i**ops_d) + ops_1):
+            if i % _ == 0:
+                ops[i] = min(ops[i], ops[_] + i // _)
+
+    return ops[n]
