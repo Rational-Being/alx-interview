b8695bfe60ce53ce3438aa07061fab11a2cf23b0
Rational-Being <overcomer021@gmail.com>
2024-05-09 09:01:34 +0100

    documentaiton

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,14 @@
-def minOperations(n):
+#!/usr/bin/python3
+"""
+minimum operations
+"""
 
+
+def minOperations(n):
+    """
+    get the least # operations needed
+    """
     if n <= 1:
         return 0
 
-    return 3 + minOperations(n-2)
+    return 3 + minOperations(n - 2)
