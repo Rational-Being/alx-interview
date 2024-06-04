973737e856a1e3cd694eacc854819789160ff195
rational-being <overcomer021@gmail.com>
2024-06-04 23:18:18 +0100

    alx interview

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+gicen a oiule of coins of different values, determine the fewest numnber of 
+coins needed to meat a given amouint of total
+"""
+
+
+def makeChange(coins, total):
+    """
+    and of course, change come from within
+    """
+
+    if total <= 0:
+        return 0
+
+    change = 0
+    amount = 0
+    coins.sort(reverse=True)
+
+    # coins = sorted(coins)[::-1]
+
+    l = len(coins)
+
+    # while l > 0:
+    #     tmp = coins[0]
+    #     if change + tmp > total:
+    #         coins.pop(0)
+    #         continue
+
+    for coin in coins:
+        while change + coin <= total:
+
+            change += coin
+            amount += 1
+
+            if change == total:
+                return amount
+    return -1
